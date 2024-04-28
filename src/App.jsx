1a196dd41c79d3ef36d7a0c3fcbff257c46dbeec
Pub <96320073+b3Rhunter@users.noreply.github.com>
2024-04-28 00:20:27 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Dad from './assets/dad.jpg'
+
+function App() {
+
+  return (
+      <div className="App">
+        <h1>Harold Eugene Sailsbury</h1>
+        <img src={Dad} alt="Dad" />
+        <p className='content'>Retired Lt. Col. Harold Eugene (Gene) Sailsbury, 71, of Pittsburg, Kan., died at 7:26 p.m. Monday, May 2, 2011, at his home following a short illness. He was born May 6, 1939, in Parsons, Kan., the son of Homer and Mildred (Hunter) Sailsbury. He graduated from Oswego High School and then went on to the University of Tulsa, and then finished at Northeastern Oklahoma A & M in Miami, Okla., with a degree in nursing. On March 2, 1977, he was united in marriage to Diane Hosman in Miami. She survives him of the home. On March 2, 1977, he was united in marriage to Diane Hosman in Miami. She survives him of the home. Gene entered the U.S. Navy on Aug. 12, 1957, and was honorably discharged in August of 1959. He later joined the U.S. Army Reserves where he spent 20 years retiring with the rank of lieutenant colonel serving with the 325th General Hospital and 313 HUS. Gene worked for the Community Health Center of Southeast Kansas, as Dr. Steven Werders nurse. He also worked for the Girard hospital as the director of nursing and retired the CEO in 2000, the Mount Carmel Medical Center and the Crawford County Mental Health and Health Department. He was a member of the Ham Radio Operators Club, past member of Pittsburg Police Reserve Officers, Past President of the National Association of Nurses and a KSNSA district officer. Additional survivors include two sons, Stanton Sailsbury and wife, Andrea, of Frontenac, Kan., and Michael Sailsbury and wife, Laurie, of McKinney, Texas; daughter, Janet Sailsbury-Cochran, of Pittsburg; three grandchildren; one great-grandchild; a nephew, Steven McCoy; and several other nieces, nephews and cousins. He was preceded in death by his parents; a twin brother, Roland Dean Sailsbury; and a sister, Shirley Kelly. Funeral services will be at 1 p.m. Friday, May 6, at the Lighthouse Tabernacle with Rev. Daryl Gaddy officiating. Burial will follow in the Highland Park Cemetery. Friends may come by the funeral home from 10 a.m. to 7 p.m. Thursday and from 8 a.m. Friday until 11 a.m. Condolences may be left at www.bathnaylor.com Memorials may be made to the Community Health Clinic of Southeast Kansas; these may be left at or mailed to the funeral home.</p>
+      </div>
+  )
+}
+
+export default App
